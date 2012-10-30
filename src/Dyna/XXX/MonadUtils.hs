bbc5cdea96b791eab8cd93878bfdb0061663abda
Nathaniel Wesley Filardo <nwf@cs.jhu.edu>
2012-10-30 02:27:10 -0400

    Add bracketState

--- src/Dyna/XXX/MonadUtils.hs
+++ src/Dyna/XXX/MonadUtils.hs
@@ -1,8 +1,20 @@
-module Dyna.XXX.MonadUtils(incState) where
+{-# LANGUAGE FlexibleContexts #-}
+
+module Dyna.XXX.MonadUtils(bracketState, incState) where
 
 import Control.Monad.State
 
-incState :: State Int Int
+bracketState :: (MonadState s m) => s -> m t -> m (t, s)
+bracketState bs m = do
+ s <- get
+ put bs 
+ r <- m
+ s' <- get
+ put s
+ return (r, bs)
+
+
+incState :: (MonadState Int m) => m Int
 incState = do
   s <- get
   put $! (s+1)
