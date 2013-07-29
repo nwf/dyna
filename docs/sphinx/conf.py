fe8bd70df737e1d7297086fdd52886122b006387
Jason Eisner <jason@cs.jhu.edu>
2013-07-29 12:46:15 -0400

    changed layout to match sphinx's own documentation, as suggested by sampledoc

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -35,8 +35,7 @@ extensions = [ 'sphinx.ext.todo'
              ]
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
-templates_path = []
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -136,8 +135,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-html_static_path = []
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
