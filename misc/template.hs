30f255b9f9a7b9de9152b6b0363d712098a55c26
Nathaniel Wesley Filardo <nwf@cs.jhu.edu>
2014-03-02 16:10:48 -0500

    Miscellany
    Add misc/template.hs as a template for new modules
    Tweak hdevtools' ghci options in misc/vimrc for Dyna.Main.Driver

---
+++ misc/template.hs
@@ -0,0 +1,29 @@
+---------------------------------------------------------------------------
+-- | SHORT DESCRIPTION OF MODULE
+--
+-- LONGER DESCRIPTION OF MODULE 
+
+-- Header material                                                      {{{
+-- {-# LANGUAGE FlexibleContexts #-}
+-- {-# LANGUAGE FlexibleInstances #-}
+-- {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- {-# LANGUAGE MultiParamTypeClasses #-}
+-- {-# LANGUAGE NoMonomorphismRestriction #-}
+-- {-# LANGUAGE OverloadedStrings #-}
+-- {-# LANGUAGE Rank2Types #-}
+-- {-# LANGUAGE ScopedTypeVariables #-}
+-- {-# OPTIONS_GHC -Wall #-}
+
+module Dyna.Foo.Bar {- (
+) -} where
+
+-- import qualified Dyna.Baz                 as DB
+-- import           Dyna.Quux
+
+------------------------------------------------------------------------}}}
+-- SECTION HEADING                                                      {{{
+
+------------------------------------------------------------------------}}}
+-- SECTION HEADING                                                      {{{
+
+------------------------------------------------------------------------}}}
